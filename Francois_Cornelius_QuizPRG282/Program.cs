8baa664334ecc3137877e3e4f03349e7e4cc16a0
Francois Cornelius <576893@student.belgiumcampus.ac.za>
2021-10-04 20:06:43 +0200

    Read from file (Step 16)

--- Francois_Cornelius_QuizPRG282/Program.cs
+++ Francois_Cornelius_QuizPRG282/Program.cs
@@ -17,8 +17,6 @@ namespace Francois_Cornelius_QuizPRG282
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            string[] users = File.ReadAllLines(@"C:\Users\fcorn\source\repos\Francois_Cornelius_QuizPRG282\Francois_Cornelius_QuizPRG282\bin\Debug\Users.txt");
-            listBox1.Items.AddRange(users);
         }
     }
 }
